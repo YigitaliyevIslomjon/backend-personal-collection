f29f7b1b710fbbd1ed5fc9fc9ab7f564e1f1c515
YigitaliyevIslomjon <77922591+YigitaliyevIslomjon@users.noreply.github.com>
2022-09-28 16:06:45 +0500

    make it better

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then((res) => {
     console.log("mongodb is connected");
@@ -25,7 +26,7 @@ app.use(cors());
 
 app.use("/api", indexRoute);
 app.use(errorHandler);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Started up at prot ${PORT}`);
 });
